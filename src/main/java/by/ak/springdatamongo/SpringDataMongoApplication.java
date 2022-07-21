c911dc5677b9b277dd776404429fa6bb64b009ab
Alexander Karankevich <givmemp3@gmail.com>
2022-07-22 00:14:45 +0200

    Initial commit

---
+++ src/main/java/by/ak/springdatamongo/SpringDataMongoApplication.java
@@ -0,0 +1,13 @@
+package by.ak.springdatamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMongoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDataMongoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/by/ak/springdatamongo/SpringDataMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ak.springdatamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
